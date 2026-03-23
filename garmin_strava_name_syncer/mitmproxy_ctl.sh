2cf54ff3ac2b59dcc730781598d56d0014855093
Edward Shao <edwardshao@csie.io>
2026-03-23 15:58:47 +0800

    name_syncer: also wait mitmproxy-ca-cert.pem

--- garmin_strava_name_syncer/mitmproxy_ctl.sh
+++ garmin_strava_name_syncer/mitmproxy_ctl.sh
@@ -1,23 +1,40 @@
 #!/bin/sh
 
 if [ "$1" = "start" ]; then
+    MAX_WAIT_SECONDS=15
     nohup uv run mitmdump -s rewrite_addon.py --listen-port 8080 > mitmproxy.log 2>&1 &
     echo $! > mitmproxy.pid
-    # wait mitmproxy to be ready and maximum 5 seconds
+    # wait for ~/.mitmproxy/mitmproxy-ca-cert.pem to be ready
     i=0
-    while [ "$i" -lt 10 ]; do
+    while [ "$i" -lt "$MAX_WAIT_SECONDS" ]; do
+        i=$((i + 1))
+        if [ -f ~/.mitmproxy/mitmproxy-ca-cert.pem ]; then
+            break
+        fi
+        sleep 1
+    done
+    if [ "$i" -eq "$MAX_WAIT_SECONDS" ]; then
+        echo "~/.mitmproxy/mitmproxy-ca-cert.pem is not ready after $MAX_WAIT_SECONDS seconds"
+        exit 1
+    else
+        echo "~/.mitmproxy/mitmproxy-ca-cert.pem is ready"
+        cp ~/.mitmproxy/mitmproxy-ca-cert.pem .
+    fi
+
+    # wait for mitmproxy to be ready
+    i=0
+    while [ "$i" -lt "$MAX_WAIT_SECONDS" ]; do
         i=$((i + 1))
         if curl -s http://localhost:8080/ > /dev/null 2>&1; then
             break
         fi
         sleep 1
     done
-    if [ "$i" -eq 5 ]; then
-        echo "mitmproxy is not ready after 5 seconds"
+    if [ "$i" -eq "$MAX_WAIT_SECONDS" ]; then
+        echo "mitmproxy is not ready after $MAX_WAIT_SECONDS seconds"
         exit 1
     else
         echo "mitmproxy is ready"
-        cp ~/.mitmproxy/mitmproxy-ca-cert.pem .
     fi
 elif [ "$1" = "stop" ]; then
     kill $(cat mitmproxy.pid)
