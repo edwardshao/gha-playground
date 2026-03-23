2e53ed5ad3d90d52d2c932922b8883b45b853770
Edward Shao <edwardshao@csie.io>
2026-03-23 15:13:07 +0800

    name_syncer: wait mitmproxy to be ready

--- garmin_strava_name_syncer/mitmproxy_ctl.sh
+++ garmin_strava_name_syncer/mitmproxy_ctl.sh
@@ -3,6 +3,21 @@
 if [ "$1" = "start" ]; then
     nohup uv run mitmdump -s rewrite_addon.py --listen-port 8080 > mitmproxy.log 2>&1 &
     echo $! > mitmproxy.pid
+    # wait mitmproxy to be ready and maximum 5 seconds
+    i=0
+    while [ "$i" -lt 5 ]; do
+        i=$((i + 1))
+        if curl -s http://localhost:8080/ > /dev/null 2>&1; then
+            break
+        fi
+        sleep 1
+    done
+    if [ "$i" -eq 5 ]; then
+        echo "mitmproxy is not ready after 5 seconds"
+        exit 1
+    else
+        echo "mitmproxy is ready"
+    fi
 elif [ "$1" = "stop" ]; then
     kill $(cat mitmproxy.pid)
     rm mitmproxy.pid
